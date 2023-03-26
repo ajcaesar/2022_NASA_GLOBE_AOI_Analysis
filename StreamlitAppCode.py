d11b2dae5a614b696500792d23941dc84099d653
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-03-26 19:57:13 -0400

    Update StreamlitAppCode.py

--- StreamlitAppCode.py
+++ StreamlitAppCode.py
@@ -8,9 +8,6 @@ st.title('Which AOIs are complete')
 df = pd.read_csv("AOI Updated Comparisons  - imported from collect earth online-2.csv")
 dfcheck = pd.DataFrame(columns = ['AOI', 'NumCompleted'])
 
-x = df.iloc[5, 7]
-st.write(pd.isna(x))
-
 i = 0
 while i < 110:
   x = 0
