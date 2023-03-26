a93e77c034689b78914edbfa3c1abb0806befafc
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-03-26 19:38:42 -0400

    Update StreamlitAppCode.py

--- StreamlitAppCode.py
+++ StreamlitAppCode.py
@@ -8,7 +8,7 @@ df = pd.read_csv("AOI Updated Comparisons  - imported from collect earth online-
 dfcheck = pd.DataFrame(columns = ['AOI', 'NumCompleted'])
 
 st.write(df.iloc[5, 7])
-st.write(df.iloc[5, 7] == nan)
+st.write(df.iloc[5, 7] == 'nan')
 #i = 0
 #while i < 110:
  # x = 0
