55a9be14d3c3c78ba7f44b5e9dd1141b3b9039b0
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-03-26 19:35:33 -0400

    Update StreamlitAppCode.py

--- StreamlitAppCode.py
+++ StreamlitAppCode.py
@@ -5,3 +5,13 @@ from io import StringIO
 
 st.title('Which AOIs are complete')
 df = pd.read_csv("AOI Updated Comparisons  - imported from collect earth online-2.csv")
+dfcheck = pd.DataFrame(columns = ['AOI', 'NumCompleted'])
+
+st.write(df.iloc[5, 7]
+#i = 0
+#while i < 110:
+ # x = 0
+  #numCompleted = 0
+  #Finished = False
+ # while x < 37
+  #df.iloc[37 * i + x, 7] != 
