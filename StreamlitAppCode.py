3cc7a0f876f4272a616a68c1c763ab22f1510215
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-03-28 17:16:45 -0400

    Update StreamlitAppCode.py

--- StreamlitAppCode.py
+++ StreamlitAppCode.py
@@ -51,7 +51,7 @@ st.map(df2)
 st.header('Table of AOIs and Number of Completed Plots')
 st.write(dfcheck)
 st.write(g)
-ww = alt.Chart(dfTime).mark_bar().encode(x="AOI", y="Avg Time")
+ww = alt.Chart(dfTime, title = 'Average Time per plot for 68 Completed AOIs').mark_bar().encode(x="AOI", y="Avg Time")
 st.altair_chart(ww)
 st.write(dfTime)
       
