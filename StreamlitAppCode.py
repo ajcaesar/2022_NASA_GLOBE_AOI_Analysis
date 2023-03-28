80c95a2e46d1acc14389506d22e94230ce353d52
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-03-28 17:24:57 -0400

    Update StreamlitAppCode.py

--- StreamlitAppCode.py
+++ StreamlitAppCode.py
@@ -51,7 +51,7 @@ st.header('Map of 68 Complete AOIs')
 st.map(df2)
 ww = alt.Chart(dfTime, title = 'Average Time (seconds) per plot for 68 Completed AOIs').mark_bar().encode(x="AOI", y="Avg Time")
 st.altair_chart(ww)
-xx = dfTime('Avg Time').describe(include = 'all')
+xx = dfTime['Avg Time'].describe(include = 'all')
 st.write(xx)
       
   
