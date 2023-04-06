68e3a1dd82fc7615a7dd162c7aa19aad5804e85e
ajcaesar <110751030+ajcaesar@users.noreply.github.com>
2023-04-06 13:10:50 -0400

    Update StreamlitAppCode.py

--- StreamlitAppCode.py
+++ StreamlitAppCode.py
@@ -5,6 +5,7 @@ import numpy as np
 import math
 import altair as alt
 import os 
+import folium
 
 st.title('Which AOIs are complete')
 df = pd.read_csv("AOI Updated Comparisons  - imported from collect earth online-3.csv")
